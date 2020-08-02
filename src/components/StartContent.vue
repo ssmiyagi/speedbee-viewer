4091d23120635538a62d9b721e16d74d7f5866e0
oceanHuman <systemokinawa@gmail.com>
2020-08-02 22:06:53 +0900

    add DBinfo list

--- src/components/StartContent.vue
+++ src/components/StartContent.vue
@@ -1,73 +1,46 @@
 <template>
   <v-container>
-    <v-data-table :headers="headers" :items="dbData" item-key="key">
-      <!-- //todo: keyTypeでKeyをformatする -->
-      <!-- <tr @click="props.expanded = !props.expanded">
-        <td>{{ props.item.title }}</td>
-        <td>{{ format(props.item.premiere) }}</td>
-        <td>{{ props.item.rating }}</td>
-      </tr> -->
-    </v-data-table>
+    <!-- <data-list /> -->
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">設定名</th>
+            <th class="text-left">値</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in dbInfo" :key="item.key">
+            <td>{{ item.key }}</td>
+            <td>{{ item.value }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
   </v-container>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
+// import DataList from "@/components/DataList.vue";
 export default Vue.extend({
-  name: "StartContent",
-  data: () => ({
-    headers: [
-      {
-        text: "TYPE",
-        value: "type",
-        align: "left",
-        sortable: true
-      },
-      {
-        text: "Key",
-        value: "key"
-      },
-      {
-        text: "Key_TYPE",
-        value: "keyType"
-      },
-      {
-        text: "DATA_TYPE",
-        value: "dataType"
-      },
-      {
-        text: "DATA",
-        value: "data"
-      }
-    ],
-    dbData: [
-      {
-        type: "Col1",
-        dataType: "int",
-        keyType: "timestamp",
-        key: "1",
-        data: 1
-      },
-      {
-        type: "Col2",
-        dataType: "int",
-        keyType: "timestamp",
-        key: "2",
-        data: 2
-      }
-    ],
-    lazy: `https://goo.gl/jbJWmK`
-  }),
-
-  methods: {
-    format(date: Date) {
-      date = new Date(date);
-      const day = `${date.getUTCDate()}`.padStart(2, "0");
-      const month = `${date.getUTCMonth() + 1}`.padStart(2, "0");
-      const year = date.getFullYear();
-      return `${month}/${day}/${year}`;
+  components: {
+    // DataList
+  },
+  data() {
+    return {
+      dbInfo: [
+        {
+          key:"dbパス",
+          value:"c/x/x/x",
+        },
+        {
+          key:"カラム数",
+          value:"4",
+        }
+      ]
     }
-  }
+  },
 });
 </script>
