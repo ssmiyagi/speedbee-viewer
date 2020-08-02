584591b0893c9dcb3c49c440bfbe6331505575fb
oceanHuman <systemokinawa@gmail.com>
2020-08-02 22:09:53 +0900

    add components/DbInfoList.vue

---
+++ src/components/DbInfoList.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">設定名</th>
+            <th class="text-left">値</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in dbInfo" :key="item.key">
+            <td>{{ item.key }}</td>
+            <td>{{ item.value }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name:"DbInfoList",
+  data() {
+    return {
+      dbInfo: [
+        {
+          key:"dbパス",
+          value:"c/x/x/x",
+        },
+        {
+          key:"カラム数",
+          value:"4",
+        }
+      ]
+    }
+  },
+});
+</script>
--- src/components/StartContent.vue
+++ src/components/StartContent.vue
@@ -1,22 +1,7 @@
 <template>
   <v-container>
     <!-- <data-list /> -->
-    <v-simple-table>
-      <template v-slot:default>
-        <thead>
-          <tr>
-            <th class="text-left">設定名</th>
-            <th class="text-left">値</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="item in dbInfo" :key="item.key">
-            <td>{{ item.key }}</td>
-            <td>{{ item.value }}</td>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
+    <db-info-list/>    
   </v-container>
 </template>
 
@@ -24,23 +9,11 @@
 import Vue from "vue";
 
 // import DataList from "@/components/DataList.vue";
+import DbInfoList from "@/components/DbInfoList.vue";
 export default Vue.extend({
   components: {
     // DataList
-  },
-  data() {
-    return {
-      dbInfo: [
-        {
-          key:"dbパス",
-          value:"c/x/x/x",
-        },
-        {
-          key:"カラム数",
-          value:"4",
-        }
-      ]
-    }
+    DbInfoList,
   },
 });
 </script>
