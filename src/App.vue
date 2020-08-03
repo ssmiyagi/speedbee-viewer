d41d3c29399e3b7cc7cf7cc24028eacc4f221f38
ssmiyagi <miyagi@saltyster.com>
2020-08-03 17:18:37 +0900

    use app accessor module

--- src/App.vue
+++ src/App.vue
@@ -12,17 +12,18 @@ import Home from './views/Home.vue';
 import VueCompositionApi, { provide } from "@vue/composition-api";
 import { DataAccessorSybol,DataAccessor } from '@/front_lib/DataAccessor';
 import { SeverDataAccessor } from '@/front_lib/SeverDataAccessor';
+import  AppDataAccessor from '@/front_lib/AppDataAccessor';
 Vue.use(VueCompositionApi);
 export default Vue.extend({
   name: 'App',
   setup(){
-    const db: DataAccessor = new SeverDataAccessor();
-    // let db: DataAccessor;
-    // if(!process.env.IS_APP){
-      // db = new SeverDataAccessor() ;
-    // }else{
-      // 
-    // }
+    let db: DataAccessor;
+    if(!process.env.IS_APP){
+       db = new SeverDataAccessor() ;
+     }else{
+       db = new AppDataAccessor() ;
+      //  db = new SeverDataAccessor() ;
+    }
     provide<DataAccessor>(DataAccessorSybol,db);
     return;
   },
